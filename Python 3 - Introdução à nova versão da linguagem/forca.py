92aa26a5d01bd9d99b940a9b4747f93df86dd6c3
Lucas Lima <69853124+lucasllimati@users.noreply.github.com>
2021-05-15 13:39:47 -0300

    exercicios

--- Python 3 - Introdução à nova versão da linguagem/forca.py
+++ Python 3 - Introdução à nova versão da linguagem/forca.py
@@ -3,6 +3,23 @@ def jogar():
     print("Bem vindo no jogo de Forca")
     print("*" * 30)
 
+    palavraSecreta = "BANANA"
+    enforcou = False
+    acertou = False
+
+    # Enquanto True
+    while (not enforcou and not acertou):
+        chute = input("Qual letra? ").upper().strip()
+
+        index = 0
+        for letra in palavraSecreta:
+            if (chute == letra):
+                print(f'Encontrei a letra {chute} na posição {index}.')
+            index += 1
+
+        print("Jogando...")
+    print(palavra2)
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
